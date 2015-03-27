312584d2287c2a9d43f865866a7f9b71824d49c4
Cherub Kumar <ckumar@zendesk.com>
2015-03-26 19:42:50 -0500

    Add and test node-zendesk module
    - Using `help center: true` in the createClient method is only possible
    after making a custom code change in the node-zendesk package. Needs to
    be addressed with a pull request eventually or forking the wrapper in
    the meantime.

---
+++ app/hc/hc_export.js
@@ -0,0 +1,20 @@
+var zendesk = require('node-zendesk'),
+    fs      = require('fs');
+
+var client = zendesk.createClient({
+  username:  'cherub.kumar@gmail.com',
+  token:     'nkwVEwjouOmciP1uuchp86EqiOGPgLEaaZpQx8Qm',
+  subdomain: 'wtp',
+  remoteUri: 'https://wtp.zendesk.com/api/v2/',
+  // disableGlobalState: true,
+  // debug: true,
+  helpcenter: true
+});
+
+client.categories.list(function (err, req, result) {
+  if (err) {
+    console.log(err);
+    return;
+  }
+  console.log(JSON.stringify(result[0], null, 2, true));//gets the first page
+});

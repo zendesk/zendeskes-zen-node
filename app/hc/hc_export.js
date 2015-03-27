36abb014eca8ee46f53a5b324c497d0c734707a3
Cherub Kumar <ckumar@zendesk.com>
2015-03-26 21:49:08 -0500

    Fetch all articles, sections and categories

--- app/hc/hc_export.js
+++ app/hc/hc_export.js
@@ -1,19 +1,44 @@
+// Dependencies
 var zendesk = require('node-zendesk'),
-    fs      = require('fs');
+    fs      = require('fs'),
+    _ = require('underscore-node');
 
+// Initialize client
 var client = zendesk.createClient({
   username:  'cherub.kumar@gmail.com',
   token:     'nkwVEwjouOmciP1uuchp86EqiOGPgLEaaZpQx8Qm',
   subdomain: 'wtp',
   remoteUri: 'https://wtp.zendesk.com/api/v2/help_center',
   // disableGlobalState: true,
-  // debug: true
+  // debug: true,
+  helpcenter: true
 });
 
+
+// Global Variables
+var categories, sections, articles;
+
+// Fetch all Categories, Sections and Articles
 client.categories.list(function (err, req, result) {
   if (err) {
     console.log(err);
     return;
   }
-  console.log(JSON.stringify(result[0], null, 2, true));//gets the first page
+  categories = result;
+});
+
+client.sections.list(function (err, req, result) {
+  if (err) {
+    console.log(err);
+    return;
+  }
+  sections = result;
+});
+
+client.articles.list(function (err, req, result) {
+  if (err) {
+    console.log(err);
+    return;
+  }
+  articles = result;
 });
